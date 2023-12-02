5de3b22127965a634037099626f563252a91b6e6
rockRayhan <khayrulalamdict@gmail.com>
2023-12-02 13:19:20 +0600

    codeignitor

--- app/Database/Migrations/2023-12-02-065606_Products1Migration.php
+++ app/Database/Migrations/2023-12-02-065606_Products1Migration.php
@@ -8,11 +8,45 @@ class Products1Migration extends Migration
 {
     public function up()
     {
-        //
+      
+	
+	
+	
+	
+	
+	 
+
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'product' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'price' => [
+                'type' => 'TEXT',
+
+            ],
+            'sku' => [
+                'type' => 'varchar',
+                'constraint' => '50',
+            ],
+            'model' => [
+                'type' => 'varchar',
+                'constraint' => '50',
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('product1');
+
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('product1');
     }
 }
